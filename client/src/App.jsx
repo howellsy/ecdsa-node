46206328d382b2dfc3e8b69240346f5a43ee74d9
Jamie Howells <jamie.howells@gmail.com>
2023-01-05 16:14:41 +0000

    App updates to store private key and signature

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ import { useState } from "react";
 function App() {
   const [balance, setBalance] = useState(0);
   const [address, setAddress] = useState("");
+  const [privateKey, setPrivateKey] = useState("");
+  const [signature, setSignature] = useState("");
 
   return (
     <div className="app">
@@ -14,8 +16,12 @@ function App() {
         setBalance={setBalance}
         address={address}
         setAddress={setAddress}
+        privateKey={privateKey}
+        setPrivateKey={setPrivateKey}
+        signature={signature}
+        setSignature={setSignature}
       />
-      <Transfer setBalance={setBalance} address={address} />
+      <Transfer setBalance={setBalance} address={address} privateKey={privateKey} />
     </div>
   );
 }
