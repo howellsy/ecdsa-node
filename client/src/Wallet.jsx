a79222acb651110fa4968b64140ee887c31d1a02
Jamie Howells <jamie.howells@gmail.com>
2023-01-05 16:19:38 +0000

    Collect private key and update balance in wallet

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,26 +1,43 @@
 import server from "./server";
+import * as secp from "ethereum-cryptography/secp256k1";
+import { toHex } from "ethereum-cryptography/utils";
+import { useEffect } from "react";
 
-function Wallet({ address, setAddress, balance, setBalance }) {
-  async function onChange(evt) {
-    const address = evt.target.value;
-    setAddress(address);
+function Wallet({ address, setAddress, balance, setBalance, privateKey, setPrivateKey }) {
+  useEffect(() => {
     if (address) {
-      const {
-        data: { balance },
-      } = await server.get(`balance/${address}`);
-      setBalance(balance);
+      (async () => {
+        const {
+          data: { balance },
+        } = await server.get(`balance/${address}`);
+        setBalance(balance);
+      })();
     } else {
       setBalance(0);
     }
-  }
+  }, [address]);
+
+  const setValue = (setter) => (evt) => setter(evt.target.value);
 
   return (
     <div className="container wallet">
       <h1>Your Wallet</h1>
 
       <label>
-        Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+        Address:
+        <input 
+          placeholder="Type an address"
+          value={address}
+          onChange={setValue(setAddress)}>
+        </input>
+      </label>
+      <label>
+        Private key:
+        <input 
+          placeholder="Type a private key"
+          value={privateKey}
+          onChange={setValue(setPrivateKey)}>
+        </input>
       </label>
 
       <div className="balance">Balance: {balance}</div>
